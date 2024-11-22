513f0d27addf25814b5a87969a1091a810336919
adilou <adilperso@Macbook-Air-Adil.local>
2024-11-22 12:19:05 +0100

    🔧 fixed route

--- app/api/send/route.tsx
+++ app/api/send/route.tsx
@@ -10,13 +10,13 @@ import { Resend } from 'resend';
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL || '';
 
-import { NextApiRequest } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 
-export async function POST(req: NextApiRequest) {
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
   const { body } = req;
-  const { email, subject, message } = body
-  try {
+  const { email, subject, message } = body;
 
+  try {
     const emailHtml = render(
       <EmailTemplate>
         <div>
@@ -29,18 +29,21 @@ export async function POST(req: NextApiRequest) {
     );
 
     const { data, error } = await resend.emails.send({
-      from: fromEmail,
+      from: fromEmail, // Replace with the actual sender email
       to: ['yakdi.adil@gmail.com', email],
       subject: subject,
       react: emailHtml,
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return res.status(500).json({ error });
     }
 
-    return Response.json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    if (error instanceof Error) {
+      return res.status(500).json({ error: error.message || 'Something went wrong' });
+    }
+    return res.status(500).json({ error: 'Something went wrong' });
   }
 }
